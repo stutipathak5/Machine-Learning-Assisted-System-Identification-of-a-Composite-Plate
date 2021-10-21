177433a5bbac1c67d38636bc28876664b8514bee
stutipathak5 <92648447+stutipathak5@users.noreply.github.com>
2021-10-21 16:47:15 +0530

    Update newmark_intg.py

--- newmark_intg.py
+++ newmark_intg.py
@@ -5,11 +5,10 @@ def NEWMARK(MG,CG,KG,t,xi,vi,fi):
   import numpy as np 
 
   dt = t[1]-t[0]                                      
-  # F=fi[:,0]                                   
-  # ai=np.dot(np.linalg.inv(MG),(F-np.dot(CG,vi)-np.dot(KG,xi)))
   ai = np.zeros((np.shape(xi)[0]))                      
   alpha = 0.25 
-  delta = 0.5                  
+  delta = 0.5   
+  
   a0 = 1/(alpha*dt**2)
   a1 = delta/(alpha*dt)                  
   a2 = 1/(alpha*dt) 
@@ -17,11 +16,13 @@ def NEWMARK(MG,CG,KG,t,xi,vi,fi):
   a4 = (delta/alpha)-1 
   a5 = (dt/2)*(delta/alpha-2)              
   a6 = dt*(1-delta) 
-  a7 = delta*dt                         
+  a7 = delta*dt   
+  
   Kb = KG+a0*MG+a1*CG 
   x = np.zeros((np.shape(xi)[0],np.shape(t)[0]))
   v = np.zeros((np.shape(xi)[0],np.shape(t)[0]))
   a = np.zeros((np.shape(xi)[0],np.shape(t)[0])) 
+  
   x[:,0] = xi
   v[:,0] = vi
   a[:,0] = ai  
